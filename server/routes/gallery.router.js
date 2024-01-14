165a1c4f9d61236a01e1408dcbac6e5494a1485e
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-14 16:11:13 -0600

    added delete endpoint in server

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -3,6 +3,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+router.delete('/delete/:id', (req, res) => {
+  const queryText = `
+    DELETE FROM "gallery" WHERE "id" = $1;
+  `
+  pool.query(queryText, [req.params.id])
+    .then((result) => {
+      console.log('successfuly DELETE');
+      res.sendStatus(200)
+    }).catch((err) => {
+      console.log(err);
+      res.sendStatus(500)
+    })
+})
+
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   const queryText = `
