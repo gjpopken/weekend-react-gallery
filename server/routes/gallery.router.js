7a7b425f4c023cf0bc9a53b09ba8188a3bf80e42
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-14 19:57:23 -0600

    got the endpoint to upload the file and check to make sure its an image

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const fs = require('fs')
+const multer = require('multer')
+const upload = multer({ dest: './public/images' })
 // const { Pool } = require('pg');
 const router = express.Router();
 const pool = require('../modules/pool')
+
+const checkFileType = (req, res, next) => {
+  console.log(req.file.mimetype);
+  const fileType = req.file.mimetype
+  if (fileType === 'image/png' || fileType === 'image/jpeg') {
+    next()
+  } else {
+    fs.unlink(req.file.path, () => {
+      console.log('removed file.');
+    })
+    res.sendStatus(403)
+  }
+}
 
 router.delete('/delete/:id', (req, res) => {
   const queryText = `
@@ -43,21 +59,43 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
-  const queryText = `
+// ! Old Working Post
+// router.post('/', (req, res) => {
+//   const queryText = `
+//   INSERT INTO "gallery" 
+// ("url", "title", "description")
+// VALUES
+// ($1, $2, $3);
+//   `
+//   const queryParams = [req.body.url, req.body.title, req.body.description]
+//   pool.query(queryText, queryParams)
+//   .then((result) => {
+//     // console.log(result);
+//     res.sendStatus(201)
+//   }).catch((err) => {
+//     console.log(err);
+//   })
+// })
+
+router.post('/', upload.single('theUpload'), checkFileType, (req, res) => {
+  console.log(req.file);
+  console.log('requ body', req.body);
+    const queryText = `
   INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
 ($1, $2, $3);
   `
-  const queryParams = [req.body.url, req.body.title, req.body.description]
-  pool.query(queryText, queryParams)
-  .then((result) => {
-    // console.log(result);
-    res.sendStatus(201)
-  }).catch((err) => {
-    console.log(err);
-  })
+  // const url = `images/${req.file.filename}`
+  const queryParams = [req.file.path, req.body.title, req.body.description]
+  //   pool.query(queryText, queryParams)
+  // .then((result) => {
+  //   // console.log(result);
+  //   res.sendStatus(201)
+  // }).catch((err) => {
+  //   console.log(err);
+  // })
+  res.sendStatus(201)
 })
 
 module.exports = router;
